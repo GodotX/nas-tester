d157fa10cca7d21fb5cae72e55169c7fbaed232b
jsievert <jsievert@4e869d7d-0b1c-0410-afa4-29d0e9ff533c>
2006-08-28 17:31:18 +0000

    Changing the server to use ncurses

--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import socket, traceback
+import curses, random, time
+
+scr = curses.initscr()
 
 host = ''
 port = 51432
@@ -10,12 +13,23 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
 
-while 1:
-	try:
+try:
+	scr.nodelay(1)
+	scr.leaveok(0)
+	max_y, max_x = scr.getmaxyx()
+
+	cur_y = 0
+	cur_x = 0
+	scr.addstr(cur_y, cur_x, 'Press Ctrl-C to quit')
+	cur_y = cur_y + 1
+	while 1:
+		c = scr.getch()
+		if c == ord('q'): break
 		message, address = s.recvfrom(8192)
-		print "Got data from", address
+		scr.addstr(cur_y, cur_x, "Got data from %s " % (address,))
 		s.sendto("I am here", address)
-	except (KeyboardInterrupt, SystemExit):
-		raise
-	except:
-		traceback.print_exc()
+		cur_y = cur_y + 1
+		scr.move(0,0)
+		scr.refresh()
+finally:
+	curses.endwin()
