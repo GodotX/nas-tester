0f1ddd5abd1c47bc1cf6b8cd9014735a51e7d954
jsievert <jsievert@4e869d7d-0b1c-0410-afa4-29d0e9ff533c>
2006-08-30 02:28:25 +0000

    First attempt at handeling client connections

--- server/server.py
+++ server/server.py
@@ -19,9 +19,10 @@ con.commit()
 host = ''
 bcastport = 9700
 cport = 9701
-
+maxClients = 20
 
 class bcastlistener(threading.Thread):
+	global db
 	def run(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,18 +32,43 @@ class bcastlistener(threading.Thread):
 		
 			message, address = s.recvfrom(8192)
 			print "Got message from %s" % (address,)
+			messageLock.acquire()
 			#scr.addstr(cur_y, cur_x, "Got data from %s " % (address,))
 			s.sendto("I am here", address)
+			messageLock.release()
 			#cur_y = cur_y + 1
 			#scr.move(0,0)
 			#scr.refresh()
+
+class dbWriter(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.setDaemon(1)
+	def run(self):
+		print "help"
+		
+class work(threading.Thread):
+	def __init__(self,clientsock):
+		threading.Thread.__init__(self)
+		self.myclientcon = clientsock
+		self.setDaemon(1)
+		self.start()
+	def run(self):
+		print self.myclientcon
+		
+		
+messageLock = threading.Lock()
 bcast_t = bcastlistener()
 bcast_t.setDaemon(1)
 bcast_t.start()
+db_t = dbWriter().start()
 
-while 1:
-	print "doing something else"
-	time.sleep(random.randint(10, 100) / 1000.0)
+lstn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lstn.bind((host, cport))
+lstn.listen(maxClients)
+for i in range(maxClients):
+	(clientcon,ap) = lstn.accept()
+	work(clientcon)
 
 
 
