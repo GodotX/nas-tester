3826de622dbd702a6f5c71acc8df4ca3257a5070
bhs128 <bhs128@4e869d7d-0b1c-0410-afa4-29d0e9ff533c>
2006-08-30 01:55:55 +0000

    added timeout and retry for the discovery process

--- trunk/client/client.py
+++ trunk/client/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket, sys
+import time, socket, sys
 dest = ('<broadcast>', 9700)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,8 +8,20 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.sendto("Hello", dest)
 print "Looking for replys; press CTRL-C to stop."
 
-while 1:
+s.settimeout(10.0)
+
+try:
 	(buf, address) = s.recvfrom(2048)
-	if not len(buf):
-		break
-	print "Received from %s: %s" % (address, buf)
+except socket.error, e:
+	print "It %s. Retrying in 5 seconds..." % e
+	time.sleep(5)
+	s.settimeout(20.0)
+	s.sendto("Hello", dest)
+	try:
+	    (buf, address) = s.recvfrom(2048)
+	except socket.error, e:
+		print "It %s. I quit." % e
+		sys.exit(1)
+
+
+print "Received from %s: %s" % (address, buf)
